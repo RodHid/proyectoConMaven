9a358cf580f1d3d055deecd2e4eb0313182e6c92
Rodrigo A. Hidalgo <rodhid00@gmail.com>
2023-11-16 23:11:37 -0600

    Creacion de dao y conexion a DB

---
+++ src/main/java/com/itca/conexion/conexion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.itca.conexion;
+
+/**
+ * @ClassName: conexion
+ * @Package: com.itca
+ * @Date: Nov 16, 2023
+ * @CopyRigth: Rodrigo Hidalgo
+ * @author Rodrigo Hidalgo
+ */
+public class conexion {
+
+}
